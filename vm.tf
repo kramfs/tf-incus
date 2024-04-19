f728c6a36be47a6751aef26b3588fde5b616ea00
Rudel Saldivar <rudelsaldivar@gmail.com>
2024-04-19 20:30:18 +0800

    Initial commit

--- vm.tf
+++ vm.tf
@@ -1,39 +1,4 @@
 ## Standard VM ##
-resource "incus_instance" "tf-incus-vm" {
-  #count   = var.instance.install ? 1 : 0
-  count   = false ? 1 : 0
-  name     = "tf-incus-vm"
-  image    = "images:ubuntu/23.10"    # To list all available image: "incus image list images:"
-  type     = "virtual-machine" # Option: container, virtual-machine. Defaults: container
-  profiles = ["${incus_profile.tf-profile[0].name}"]
-  running = true
-
-  ## REF: https://linuxcontainers.org/incus/docs/main/reference/instance_options/
-  config = {
-    "boot.autostart" = false
-    "user.access_interface" = "eth0"
-  }
-
-  #device {
-  #  name = "root"
-  #  type = "disk"
-  #  properties = {
-  #    pool = "default"
-  #    size = "30GiB"
-  #    path = "/"
-  #  }
-  #}
-
-  ## REF: https://linuxcontainers.org/incus/docs/main/reference/instance_options/#instance-resource-limits
-  limits = {
-    cpu    = 2
-    memory = "8GiB" # Percentage of the host’s memory or a fixed value in bytes. Various suffixes are supported.
-    #memory.enforce = "hard" # Default: hard. Whether the memory limit is hard or soft
-  }
-}
-
-
-## Custom VM: Initailly Boot from ISO Installer ##
 resource "incus_instance" "tf-incus-vm-iso" {
   count   = var.instance.install ? 1 : 0
   name     = "tf-incus-vm-iso"
